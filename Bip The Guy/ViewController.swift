774d23b95c3bd46f7f8ece5da6daf382431a4509
Hiroki <ll@lls-macbook-pro.local>
2019-02-10 21:45:38 -0500

    4.4 completed

--- Bip The Guy/ViewController.swift
+++ Bip The Guy/ViewController.swift
@@ -7,11 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 //Mark PROPERTIES
     @IBOutlet weak var imageToPunch: UIImageView!
     
+    var audioPlayer = AVAudioPlayer ()
+    var imagePicker = UIImagePickerController ()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,16 +32,56 @@ class ViewController: UIViewController {
                                           height: self.imageToPunch.bounds.size.height - shrinkValue)
         UIView.animate(withDuration: 0.25, delay: 0.0, usingSpringWithDamping: 0.2, initialSpringVelocity: 10, options: [], animations: {self.imageToPunch.bounds = bounds}, completion: nil)
     }
+    func playSound (soundName: String, audioPlayer: inout AVAudioPlayer) {
+        if let sound = NSDataAsset(name: soundName) {
+            do {
+                try audioPlayer = AVAudioPlayer(data: sound.data)
+                audioPlayer.play()
+            } catch {
+                print("ERROR: data in \(soundName) couldn't be played as a sound.")
+            }
+        } else {
+            print("ERROR: file \(soundName) didn't load")
+        }
+    }
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    
+//APPLE GIVES YOU A DICTIONARY "info" WHERE YOU COULD PULL OUT STUFF, IN THIS CASE, THE SELECTED PHOTOS
+        let selectedImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        imageToPunch.image = selectedImage
+        dismiss(animated: true, completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
 //MARK ACTIONS
     @IBAction func libraryPressed(_ sender: UIButton) {
+//SHOWS PHOTO LIBRARY WHEN PRESSED
+        imagePicker.sourceType = .photoLibrary
+//IMAGE CAN COMMUNICATE WITH VIEWCONTROLLER WHICH IS DEFINED AS "SELF" HERE
+        imagePicker.delegate = self
+        present(imagePicker, animated: true, completion: nil)
     }
-    
+    func showAlert(title: String, message: String) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alertController.addAction(defaultAction)
+        present(alertController, animated: true, completion: nil)
+        
+    }
     @IBAction func cameraPressed(_ sender: UIButton) {
-    }
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            imagePicker.sourceType = .camera
+            imagePicker.delegate = self
+            present(imagePicker, animated: true, completion: nil)
+        } else {
+           showAlert(title: "Camera Not Available", message: "There is no camera available on this device.")
+        }
+        }
+    
     @IBAction func imageTapped(_ sender: UITapGestureRecognizer) {
-        print("You Pressed the Image")
         animateImage()
+        playSound(soundName: "punchSound", audioPlayer: &audioPlayer)
     }
     
 }
-
